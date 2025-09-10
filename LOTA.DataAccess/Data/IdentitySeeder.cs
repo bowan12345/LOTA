f959ae3e8cda00bb95ad096e8305a12bef497c8c
bowan12345 <bowan.ding@gmail.com>
2025-09-10 12:00:01 +1200

    update admin account's setting, read from the azure enviroment

--- LOTA.DataAccess/Data/IdentitySeeder.cs
+++ LOTA.DataAccess/Data/IdentitySeeder.cs
@@ -1,6 +1,7 @@
 ﻿using LOTA.Model;
 using LOTA.Utility;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,7 @@ namespace LOTA.DataAccess.Data
             using var scope = serviceProvider.CreateAsyncScope();
             var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var configuration = scope.ServiceProvider.GetService<IConfiguration>();
 
             string[] roles = { Roles.Role_Admin, Roles.Role_Tutor, Roles.Role_Student };
             foreach (var role in roles)
@@ -28,11 +30,22 @@ namespace LOTA.DataAccess.Data
                 }
             }
 
-            // 2. initial admin account
-            string adminEmail = "admin@weltec.ac.nz";
-            string adminPassword = "Admin123!"; 
-            var adminUser = userManager.FindByEmailAsync(adminEmail).GetAwaiter().GetResult();
+            // 2. initial admin account read from configuration on Azure app setting
+            string adminEmail = configuration?["Admin:Email"] ?? "admin@weltec.ac.nz";
+            string adminPassword = configuration?["Admin:Password"] ?? "Admin123!"; 
+            //read force reset switch on Azure app setting
+            bool forceReset = bool.TryParse(configuration?["Admin:ForceReset"], out bool resetValue) && resetValue;
+            // if force reset is true, then delete all previous admin account data 
+            if (forceReset)
+            {
+                var oldAdmins = userManager.GetUsersInRoleAsync(Roles.Role_Admin).GetAwaiter().GetResult();
+                foreach (var oldAdmin in oldAdmins)
+                {
+                    userManager.DeleteAsync(oldAdmin).GetAwaiter().GetResult();
+                }
+            }
 
+            var adminUser = userManager.FindByEmailAsync(adminEmail).GetAwaiter().GetResult();
             if (adminUser == null)
             {
                 var newAdmin = new ApplicationUser
@@ -40,8 +53,8 @@ namespace LOTA.DataAccess.Data
                     UserName = adminEmail,
                     Email = adminEmail,
                     EmailConfirmed = true,
-                    FirstName = "Admin",
-                    LastName = "Weltec",
+                    FirstName = configuration?["Admin:FirstName"] ?? "Admin",
+                    LastName = configuration?["Admin:LastName"] ?? "Weltec",
                     IsActive = true,
                     SecurityStamp = Guid.NewGuid().ToString()
                 };
